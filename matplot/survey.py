550e3339aa20565c0961bdb8dbf2fad8891a49a2
Christopher Rooney <ctr_287@sbcglobal.net>
2015-05-05 10:19:19 -0500

    Initial generation logic for survey origins

--- matplot/survey.py
+++ matplot/survey.py
@@ -1,8 +1,10 @@
 import common
 import numpy as np
 import pylab
+import math
 import scipy.optimize as optimize
 import matplotlib.backends.backend_pdf as pdfback
+import itertools
 
 np.seterr(all='raise')
 #When having problems with dividing by zero, we can debug more easily by having execution
@@ -57,9 +59,15 @@ def singlerun(filename,outputFile,binsize,chop):
     pylab.title("Distribution of Galaxy Distance")
     pylab.legend()
 
+    fig2 = pylab.figure()
+    shellVolume = [((4/3)*math.pi*(robot.centerbins[i])**3-((4/3)*math.pi*(robot.centerbins[i-1])**3 if i > 0 else 0)) for i in range(len(n))]
+    print(shellVolume)
+    density = [n[i]/shellVolume[i] for i in range(len(n))]
+    pylab.plot(robot.centerbins,density)
     #Save figure
     with pdfback.PdfPages(outputFile+str(binsize)) as pdf:
         pdf.savefig(fig)
+        pdf.savefig(fig2)
     params = robot.result.x
     #Write paramaters to a file for later use.
     common.writedict(outputFile+str(binsize)+'_params.json',{'A':params[0],
@@ -133,18 +141,77 @@ def selectrun(args):
     #either need a way of generating random points that are a certain minimum distance from each other or I'll
     #need to manually make a list of a bunch of points to use, then recycle them. The first method would be
     #better, under some circumstances probably.
+    settings = common.getdict(args.settings)
+    
+    hugeFile       = settings["dataset_filename"]
+    density        = settings['dataset_density']
+    surveyOverride = settings['survey_position_override']
+    boxSize        = settings['box_size']
+    if surveyOverride is not None:
+        surveys = surveyOverride
+    else:
+        surveySeparation = settings['survey_separation_distance']
+        numSurveys       = settings['num_surveys']
+        surveys = genSurveyPos(surveySeparation, boxSize, numSurveys)
+    
+    selectionParams = common.getdict(settings['selection_function_json'])
     with open(FILENAME, 'r') as theFile:
         for i,rawline in enumerate(theFile):
             if i != 0:
                 line = rawline.strip()
                 row = line.split(',')
-                surveyCheck(row, surveys)
+                surveyCheck(row, surveys, selectionParams)
 
-def surveyCheck(info, surveys):
+def surveyCheck(info, surveys, params):
     #This function should go through all the surveys and determine the distance from the data point (info) to the
-    #center of the survey, then 
+    #center of the survey, then figure out the probability that you should pick the point in question.
+    selection = lambda x: selection_function(x,**params)
+    #selection is the specific version of the generalized selection function, a.k.a. the selection function
+    #with all the constants set to their values.
+    x = info[0]
+    y = info[1]
+    z = info[2]
+    distances = [sqrt( (x-r[0])**2 +
+                       (y-r[1])**2 +
+                       (z-r[2])**2 ) for r in surveys]
     
 
-
+def genSurveyPos(separation, boxsize, numSurveys):
+    surveys = []
+    numCatches = 0
+    assert numSurveys > 0
+    if not numSurveys*separation**3 < (boxsize[0] - separation)*(boxsize[1] - separation)*(boxsize[2] - separation):
+        print("[WARN] there may be too many surveys for this box size and separation distance.")
+    rng = np.random.RandomState()
+    for i in range(numSurveys):
+        while True:
+            #Note: this is basically the equivalent of bogosort as far as algorithm efficiency
+            #is concerned. If you try to cram too many surveys into a box it *WILL* keep on running forever.
+            #So don't do that. The assert statements should keep a reasonable amount of 
+            edgeBound = separation/2
+            surveyCoord = (rng.uniform(edgeBound,boxsize[0]-edgeBound),
+                           rng.uniform(edgeBound,boxsize[1]-edgeBound),
+                           rng.uniform(edgeBound,boxsize[2]-edgeBound))
+            distances = [math.sqrt((r1[0]-r2[0])**2+
+                                   (r1[1]-r2[1])**2+
+                                   (r1[2]-r2[2])**2)
+                             for r1,r2 in zip(itertools.repeat(surveyCoord),surveys)]
+            if all(distance > separation for distance in distances):
+                #All is officially the 'coolest function ever.' All of an empty list is true!
+                surveys.append(surveyCoord)
+                break
+            else:
+                numCatches += 1
+            if numCatches > 500000:
+                print("We tried SO HARD to make this survey for you, but it just didn't work.")
+                print("We're very sorry. We might still be able to make you a survey if you reduce")
+                print("the number of surveys that we have to put into this box and try again.")
+                print("")
+                print("Sincerely,")
+                print("The elves that sit in your laptop doing hundreds of math problems per second.")
+                exit()
+    print("Caught {}!".format(numCatches))
+    return surveys
+    
 if __name__ == "__main__":
     print("This does not run standalone.")
