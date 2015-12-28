4bbb10b00c8ea2c8b3059bf4b8beb2e5874da4ba
Christopher Rooney <ctr_287@sbcglobal.net>
2015-12-28 09:13:38 -0600

    added distance (not modulus) perturbations

--- matplot/perturb.sh
+++ matplot/perturb.sh
@@ -1,3 +1,5 @@
+#This isn't really a shell script... I wouldn't recommend running it. Just use it to stage commands and copy-paste them
+
 python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-perturbed-0.1-{}.dat 0.1 100
 python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-perturbed-0.2-{}.dat 0.2 100
 python survey_perturb.py surveys/100s/COMPOSITE-69-survey.dat surveys/perturbed/COMPOSITE-29-perturbed-0.1-{}.dat 0.1 100
@@ -10,3 +12,19 @@ python survey_perturb.py surveys/100s/CF2-group-83-survey.dat surveys/perturbed/
 python survey_perturb.py surveys/100s/CF2-group-83-survey.dat surveys/perturbed/CF2-group-83-perturbed-0.2-{}.dat 0.2 100
 python survey_perturb.py surveys/100s/CF2-group-38-survey.dat surveys/perturbed/CF2-group-38-perturbed-0.1-{}.dat 0.1 100
 python survey_perturb.py surveys/100s/CF2-group-38-survey.dat surveys/perturbed/CF2-group-38-perturbed-0.2-{}.dat 0.2 100
+
+
+
+
+python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-p-0.1-naive-{}.dat 0.1 100 -n
+python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-p-0.2-naive-{}.dat 0.2 100 -n
+python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-p-0.1-notmod-{}.dat 0.1 100 -d
+python survey_perturb.py surveys/100s/COMPOSITE-1-survey.dat surveys/perturbed/COMPOSITE-1-p-0.2-notmod-{}.dat 0.2 100 -d
+python survey_perturb.py surveys/100s/COMPOSITE-69-survey.dat surveys/perturbed/COMPOSITE-29-p-0.1-naive-{}.dat 0.1 100 -n
+python survey_perturb.py surveys/100s/COMPOSITE-69-survey.dat surveys/perturbed/COMPOSITE-29-p-0.2-naive-{}.dat 0.2 100 -n
+python survey_perturb.py surveys/100s/COMPOSITE-69-survey.dat surveys/perturbed/COMPOSITE-29-p-0.1-notmod-{}.dat 0.1 100 -d
+python survey_perturb.py surveys/100s/COMPOSITE-69-survey.dat surveys/perturbed/COMPOSITE-29-p-0.2-notmod-{}.dat 0.2 100 -d
+
+
+
+
--- matplot/survey_perturb.py
+++ matplot/survey_perturb.py
@@ -5,6 +5,7 @@ import math
 np.seterr(all="raise")
 def perturb(args):
     num_acks = 0
+    second_order_acks = 0
     num_errs = 0
     hubble_constant = 100
     fractional_error = args.frac_error
@@ -12,13 +13,19 @@ def perturb(args):
     perturbed_vs = []
     delta_vs = []
     for galaxy in galaxies:
-        if galaxy.v > galaxy.cz/10:
+        if abs(galaxy.v) > galaxy.cz/10:
             num_acks += 1
             continue
-        distance_modulus = np.log(galaxy.d)
-        perturbed_dmod = np.random.normal(distance_modulus,abs(distance_modulus*fractional_error),args.num)
-        skewed_distance = np.e ** perturbed_dmod
-        if args.naive:
+        if not args.distance:
+            distance_modulus = np.log(galaxy.d)
+            perturbed_dmod = np.random.normal(distance_modulus,abs(distance_modulus*fractional_error),args.num)
+            skewed_distance = np.e ** perturbed_dmod
+            dv = hubble_constant * (np.e ** (distance_modulus + distance_modulus*args.frac_error) - galaxy.d)
+        else:
+            skewed_distance = np.random.normal(galaxy.d,abs(galaxy.d*fractional_error),args.num)
+            dv = galaxy.d*fractional_error*hubble_constant
+            
+        if args.naive or args.distance:
             try:
                 velocities = galaxy.cz - hubble_constant * skewed_distance
             except FloatingPointError:
@@ -27,11 +34,16 @@ def perturb(args):
         else:
             try:
                 velocities = galaxy.cz * np.log(galaxy.cz / (hubble_constant * skewed_distance) )
+                for velocity in velocities:
+                    if abs(velocity) > galaxy.cz / 10:
+                        second_order_acks += 1
             except FloatingPointError:
                 num_errs += 1
                 continue
+
         perturbed_vs.append((velocities,galaxy))
-        delta_vs.append(np.e**(distance_modulus + distance_modulus*args.frac_error) - galaxy.d)
+        delta_vs.append(dv)
+
     for n in range(args.num):
         outCF2String = ""
         for i,pv in enumerate(perturbed_vs):
@@ -49,13 +61,14 @@ def perturb(args):
 
     print("{} out of {} galaxies ({:.2f}) had true velocity NOT much less than redshift,".format(num_acks,len(galaxies),num_acks/len(galaxies)))
     print("i.e. the condition on our estimator that v << cz was not satisfied.")
+    print("This happened to the random data {} times out of {}.".format(second_order_acks,args.num*len(galaxies)))
     print()
     print("Also, {} FloatingPoint errors happened, even after taking out the above galaxies.".format(num_errs))
 
 if __name__ == "__main__":
-    arrrghs = common.parseCmdArgs([['cf2file'],['outfile'],['frac_error'],['num'],['-n','--naive']],
-                                   ['CF2 survey file to perturb','Output file spec. Must contain exactly one "{}" for use in numbering.', 'Fractional error, determines the standard deviation of the normal distribution used in the distance modulus','Number of perturbed survey files to generate','Use the naive velocity estimator v = cz - H0*d'],
-                                  [str,str,float,int,'bool'])
+    arrrghs = common.parseCmdArgs([['cf2file'],['outfile'],['frac_error'],['num'],['-n','--naive'],['-d','--distance']],
+                                   ['CF2 survey file to perturb','Output file spec. Must contain exactly one "{}" for use in numbering.', 'Fractional error, determines the standard deviation of the normal distribution used in the distance modulus','Number of perturbed survey files to generate','Use the naive velocity estimator v = cz - H0*d','Don\'t use the distance modulus, just use distance (implies -n)'],
+                                  [str,str,float,int,'bool','bool'])
     perturb(arrrghs)
 
 
