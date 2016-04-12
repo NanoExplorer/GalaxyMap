05ddba41672f7ec83789ba3fbafa92bf3c7bc7b4
Christopher Rooney <ctr_287@sbcglobal.net>
2016-04-11 19:07:25 -0500

    Cosmetic changes

--- matplot/velocity_correlation.py
+++ matplot/velocity_correlation.py
@@ -25,26 +25,6 @@ import time
 import smtplib
 #import matplotlib.ticker as mtick
 
-"""
-Traceback (most recent call last):
-  File "velocity_correlation.py", line 963, in <module>
-    main(arrrghs)
-  File "velocity_correlation.py", line 99, in main
-    itertools.repeat(maxd_master))
-  File "velocity_correlation.py", line 411, in standBackStats_perfectBackground
-    std = np.std(allData,axis=0)
-  File "/home/christopher/.Envs/new-matplot/lib/python3.4/site-packages/numpy/core/fromnumeric.py", line 2985, in std
-    keepdims=keepdims)
-  File "/home/christopher/.Envs/new-matplot/lib/python3.4/site-packages/numpy/core/_methods.py", line 124, in _std
-    keepdims=keepdims)
-  File "/home/christopher/.Envs/new-matplot/lib/python3.4/site-packages/numpy/core/_methods.py", line 77, in _var
-    arr = asanyarray(a)
-  File "/home/christopher/.Envs/new-matplot/lib/python3.4/site-packages/numpy/core/numeric.py", line 525, in asanyarray
-    return array(a, dtype, copy=False, order=order, subok=True)
-ValueError: could not broadcast input array from shape (7,50) into shape (7)
-"""
-
-
 TEMP_DIRECTORY = "/media/christopher/2TB/Christopher/code/Physics/GalaxyMap/tmp/"
 PERFECT_LOCATION = "output/PERFECT_DONTTOUCH/COMPOSITE-MOCK-bin-{:.0f}-{}.npy"
 print("Warning: Non-general perfect location")
